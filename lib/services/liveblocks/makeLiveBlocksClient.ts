ec23bb24288ab29f91419e43df07eeaec40962e9
John Carmack <johncarmack@me.com>
2022-04-02 18:06:46 -0700

    updated API key so we're not hogging their junk

--- lib/services/liveblocks/makeLiveBlocksClient.ts
+++ lib/services/liveblocks/makeLiveBlocksClient.ts
@@ -2,8 +2,8 @@ import { createClient } from "@liveblocks/client";
 
 export function makeLiveBlocksClient() {
   return createClient({
-    publicApiKey: false
-      ? "pk_test_ETujS9FV5jeieP0ussc8xBkQ"
-      : "pk_live_LQJHauN3xAZFOMO6XY7fscYK",
+    publicApiKey: true
+      ? "pk_live_ISxaaHYdI1hvkYEewqHPhFWO"
+      : "pk_live_ISxaaHYdI1hvkYEewqHPhFWO",
   });
 }
---
+++ lib/tests/App.test.ts
@@ -0,0 +1,8 @@
+describe("App", () => {
+  it("loads and displays content", async () => {
+    // GIVEN
+    //const { result } = render(<App />);
+    // WHEN
+    // THEN
+  });
+});
